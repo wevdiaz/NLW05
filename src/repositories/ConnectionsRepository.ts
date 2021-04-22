1ca6e9d3ea582178191de2b55227cb7118332dfa
Weverson Diazz <wevers.info@gmail.com>
2021-04-22 20:16:13 -0300

    Create Connections do app
    Criação da tabela Connections
    
    Repository
    Services
    Entity

---
+++ src/repositories/ConnectionsRepository.ts
@@ -0,0 +1,9 @@
+import { Repository, EntityRepository } from "typeorm";
+
+
+import { Connection } from "../entities/Connection";
+
+@EntityRepository(Connection)
+class ConnectionsRepository extends Repository<Connection> {}
+
+export { ConnectionsRepository }
--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -1,7 +1,33 @@
 import { io } from "../http";
+import { ConnectionsService } from "../services/ConnectionsService";
+import { UsersService } from "../services/UsersService";
 
 io.on("connect", (socket) => {
-    socket.on("client_first_access", (params) => {
-        console.log(params);
+    const connectionsService = new ConnectionsService();
+    const usersService = new UsersService();
+
+    socket.on("client_first_access", async (params) => {
+        const socket_id = socket.id;
+        const { text, email } = params;
+
+        const userExists = await usersService.findByEmail(email);
+
+        if (!userExists) {
+            const user = await usersService.create(email);
+
+            await connectionsService.create({
+                socket_id,
+                user_id: user.id
+            });
+        }
+        else {
+            await connectionsService.create({
+                socket_id,
+                user_id: userExists.id
+            });
+        }
+        
+
+       
     });
 });
