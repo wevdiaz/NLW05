c91e3068a449dbbc9120210afc7d18ebd5b4585a
Weverson Diazz <wevers.info@gmail.com>
2021-04-22 17:44:10 -0300

    Estruturando o chat com socket.io

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,5 @@
-import express, { request, response } from "express";
-import { createServer } from "http";
-import { Server, Socket } from "socket.io";
-
-import "./database";
-import { routes } from "./routes";
-
-const app = express();
-
-const http = createServer(app);
-const io = new Server(http);
-
-io.on("connection", (socket: Socket) => {
-    console.log("Se conectou", socket.id );
-});
-
-app.use(express.json());
-
-app.use(routes);
+import {http} from "./http";
+import "./websocket/client";
 
 http.listen(3333, () => {
     console.log("Server is Working on port 3333");
